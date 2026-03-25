e1015c9247b4c32fdc5236853b43a0e190738c8d
RAGAVI274 <151647178+RAGAVI274@users.noreply.github.com>
2026-03-25 18:25:49 +0530

    Update rect.bash

--- B-Shell/rect.bash
+++ B-Shell/rect.bash
@@ -1,11 +1,13 @@
-
+echo "Area and Perimeter of the rectangle"
+echo "-------------------------------"
 echo "Enter the length:"
 read length
 
 echo "Enter the width:"
 read width
 
-
+echo"report"
+echo"------------------"
 area=$((length * width))
 
 
